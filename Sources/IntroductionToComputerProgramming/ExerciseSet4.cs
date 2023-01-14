31770514e6a2e4af1e59a5333bfccffad8a2e10c
Tomasz Jagiełka <tomasz.m.jagielka@gmail.com>
2023-01-15 00:55:06 +0100

    Add exercise 4 from exercise set 4

--- Sources/IntroductionToComputerProgramming/ExerciseSet4.cs
+++ Sources/IntroductionToComputerProgramming/ExerciseSet4.cs
@@ -32,5 +32,67 @@ namespace IntroductionToComputerProgramming
                 } while (input != "stop");
             }
         }
+
+        struct Point
+        {
+            public double x { get; set; }
+            public double y { get; set; }
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public double GetDistance(Point point)
+            {
+                return Math.Sqrt(Math.Pow(this.x - point.x, 2) + Math.Pow(this.y - point.y, 2));
+            }
+        }
+
+        struct Trapezoid
+        {
+            Point topLeft, topRight, bottomLeft, bottomRight;
+            double height;
+
+            public Trapezoid(Point topLeft, Point topRight, Point bottomLeft, Point bottomRight, double height)
+            {
+                this.topLeft = topLeft;
+                this.topRight = topRight;
+                this.bottomLeft = bottomLeft;
+                this.bottomRight = bottomRight;
+                this.height = height;
+            }
+
+            public double GetArea()
+            {
+                double a = this.topLeft.GetDistance(this.topRight);
+                double b = this.bottomLeft.GetDistance(this.bottomRight);
+                Console.WriteLine($"{a}, {b}");
+                return 0.5 * (a + b) * this.height;
+            }
+        }
+
+        public static void Exercise4()
+        {
+            string[] data = File.ReadAllText("trapezoids.csv").Split(Environment.NewLine);
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] != "")
+                {
+                    string[] values = data[i].Split(",");
+
+                    Point topLeft = new Point(double.Parse(values[0]), double.Parse(values[1]));
+                    Point topRight = new Point(double.Parse(values[2]), double.Parse(values[3]));
+                    Point bottomLeft = new Point(double.Parse(values[4]), double.Parse(values[5]));
+                    Point bottomRight = new Point(double.Parse(values[6]), double.Parse(values[7]));
+                    double height = double.Parse(values[8]);
+
+                    Trapezoid trapezoid = new Trapezoid(topLeft, topRight, bottomLeft, bottomRight, height);
+
+                    Console.WriteLine($"Area of trapezoid {i + 1}: {trapezoid.GetArea()}");
+                }
+            }
+        }
     }
 }
