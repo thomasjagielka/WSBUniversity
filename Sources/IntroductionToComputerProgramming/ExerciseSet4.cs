b94dbb1faf48ae1c70b71652f36bebf390b1ecd6
Tomasz Jagiełka <tomasz.m.jagielka@gmail.com>
2023-01-15 00:04:19 +0100

    Improve exercise 1 and 2 from exercise set 4

--- Sources/IntroductionToComputerProgramming/ExerciseSet4.cs
+++ Sources/IntroductionToComputerProgramming/ExerciseSet4.cs
@@ -13,24 +13,5 @@ namespace IntroductionToComputerProgramming
         {
             Console.Write(File.ReadAllText("my_first_file.txt"));
         }
-
-        public static void Exercise3()
-        {
-            string FILE_NAME = "data.txt";
-            if (!File.Exists(FILE_NAME))
-                File.AppendAllText(FILE_NAME, "Captain's log" + Environment.NewLine);
-
-            while (Helper.GetInput<string>() != "start") { }
-
-            string input = Helper.GetInput<string>();
-            using (StreamWriter sw = File.CreateText(FILE_NAME))
-            {
-                do
-                {
-                    sw.WriteLine(input);
-                    input = Helper.GetInput<string>();
-                } while (input != "stop");
-            }
-        }
     }
 }
